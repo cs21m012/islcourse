1722a378e9a5bf5483f4f6b2d5e7b42ed66867c9
cs21m012 <89316140+cs21m012iittp@users.noreply.github.com>
2022-10-20 13:33:43 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -115,14 +115,71 @@ def get_model_advanced(train_data_loader=None, n_epochs=10,lr=1e-4,config=None):
 # sample invocation torch.hub.load(myrepo,'test_model',model1=model,test_data_loader=test_data_loader,force_reload=True)
 def test_model(model1=None, test_data_loader=None):
   accuracy_val, precision_val, recall_val, f1score_val = 0, 0, 0, 0
+   
+  num_epochs = 5
+  count = 0
+  # Lists for visualization of loss and accuracy 
+  loss_list = []
+  iteration_list = []
+  accuracy_list = []
 
+   # Lists for knowing classwise accuracy
+  predictions_list = []
+  labels_list = []
+
+  for epoch in range(num_epochs):
+      for images, labels in train_loader:
+         # Transfering images and labels to GPU if available
+          images, labels = images.to(device), labels.to(device)
+    
+          train = Variable(images.view(100, 1, 28, 28))
+          labels = Variable(labels)
+        
+        # Forward pass 
+           outputs = model(train)
+           loss = error(outputs, labels)
+        
+        # Initializing a gradient as 0 so there is no mixing of gradient among the batches
+           optimizer.zero_grad()
+        
+        #Propagating the error backward
+           loss.backward()
+        
+        # Optimizing the parameters
+          optimizer.step()
+    
+          count += 1
+          if not (count % 50):    # It's same as "if count % 50 == 0"
+            total = 0
+            correct = 0
+        
+            for images, labels in test_loader:
+                images, labels = images.to(device), labels.to(device)
+                labels_list.append(labels)
+            
+                test = Variable(images.view(100, 1, 28, 28))
+            
+                outputs = model(test)
+            
+                predictions = torch.max(outputs, 1)[1].to(device)
+                predictions_list.append(predictions)
+                correct += (predictions == labels).sum()
+            
+                total += len(labels)
+           accuracy = correct * 100 / total
+           loss_list.append(loss.data)
+           iteration_list.append(count)
+           accuracy_list.append(accuracy) 
+        
+   #if not (count % 500):
+            #print("Iteration: {}, Loss: {}, Accuracy: {}%".format(count, loss.data, accuracy))
   
-  accuracy_val = torchmetrics.Accuracy(10)
-  precision_val = torchmetrics.Precision(10)
+  #accuracy_val = torchmetrics.Accuracy(10)
+  #precision_val = torchmetrics.Precision(10)
 
 
-  recall = torchmetrics.Recall(10)
-  score_val = torchmetrics.F1Score(10)
+  #recall = torchmetrics.Recall(10)
+  #score_val = torchmetrics.F1Score(10)
 
   # write your code here as per instructions
   # ... your code ...
@@ -132,6 +189,6 @@ def test_model(model1=None, test_data_loader=None):
   
   print ('Returning metrics... (rollnumber: CS21M012)')
   
-  return accuracy_val, precision_val, recall_val,score_val
+  return accuracy_list
 
 
